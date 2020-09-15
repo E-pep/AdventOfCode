f16cd40b404b687713857eeed26485c3f87168b8
E-pep <paul.leroy1996@hotmail.com>
2020-09-15 19:26:01 +0200

    day 3 finished

--- 2015/Day3/Day3.py
+++ 2015/Day3/Day3.py
@@ -4,9 +4,10 @@ import numpy
 # Find the amount of houses with atleast one present
 def FindHouses(file):
     TotalHouses = 1
-    CurrentPosition = [0,0]
+    CurrentPosition = [0, 0]
     ReceivedHousesPositionList = []
-    ReceivedHousesPositionList.append(CurrentPosition)
+    ReceivedHousesPositionList.append([0,0])
+    # ReceivedHousesPositionList.append(CurrentPosition)
     for line in file:
         for character in line:
             if character == '>':
@@ -17,14 +18,58 @@ def FindHouses(file):
                 CurrentPosition[1] = CurrentPosition[1] + 1
             elif character == 'v':
                 CurrentPosition[1] = CurrentPosition[1] - 1
-            print("current position:")
-            print(CurrentPosition)
-            if CurrentPosition not in ReceivedHousesPositionList:
-                ReceivedHousesPositionList.append(CurrentPosition)
+            if [CurrentPosition[0], CurrentPosition[1]] not in ReceivedHousesPositionList:
+                print("current position in if:")
+                print(CurrentPosition)
+                ReceivedHousesPositionList.append([CurrentPosition[0], CurrentPosition[1]])
     print("receivedHousePositions:")
-    print(ReceivedHousesPositionList)
+    print(len(ReceivedHousesPositionList))
+
+
+
+# part 2 find the amount of houses with help of robo-santa
+
+def FindHousesRoboSanta(file):
+    TotalHouses = 1
+    CurrentPosition = [0, 0]
+    CurrentPositionRobosanta = [0,0]
+    ReceivedHousesPositionList = []
+    ReceivedHousesPositionList.append([0,0])
+
+    # variable which switches between santa and robosanta
+    switch = False
+    for line in file:
+        for character in line:
+            if switch == False:
+                if character == '>':
+                    CurrentPosition[0] = CurrentPosition[0] + 1
+                elif character == '<':
+                    CurrentPosition[0] = CurrentPosition[0] - 1
+                elif character == '^':
+                    CurrentPosition[1] = CurrentPosition[1] + 1
+                elif character == 'v':
+                    CurrentPosition[1] = CurrentPosition[1] - 1
+
+                if [CurrentPosition[0], CurrentPosition[1]] not in ReceivedHousesPositionList:
+                    ReceivedHousesPositionList.append([CurrentPosition[0], CurrentPosition[1]])
+
+            elif switch == True:
+                if character == '>':
+                    CurrentPositionRobosanta[0] = CurrentPositionRobosanta[0] + 1
+                elif character == '<':
+                    CurrentPositionRobosanta[0] = CurrentPositionRobosanta[0] - 1
+                elif character == '^':
+                    CurrentPositionRobosanta[1] = CurrentPositionRobosanta[1] + 1
+                elif character == 'v':
+                    CurrentPositionRobosanta[1] = CurrentPositionRobosanta[1] - 1
+                if [CurrentPositionRobosanta[0], CurrentPositionRobosanta[1]] not in ReceivedHousesPositionList:
+                    ReceivedHousesPositionList.append([CurrentPositionRobosanta[0], CurrentPositionRobosanta[1]])
+            switch = not switch
+    print("receivedHousePositions:")
+    print(len(ReceivedHousesPositionList))
 
 if __name__ == "__main__":
     # open input file
     f = open("input.txt", "r")
-    FindHouses(f)
+    # FindHouses(f)
+    FindHousesRoboSanta(f)
